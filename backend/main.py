6e634ed495c0c3a28de4c66ac13911d9dfaa9e01
Vinay Kankatala <vinaykankatala19@gmail.com>
2026-04-25 18:14:08 +0530

    changed in code

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://content-moderation-git-main-vinay-kankatalas-projects.vercel.app"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
